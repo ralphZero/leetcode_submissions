c4d6e559e14af94f1108b7ed4cd6525d41408086
ralphZero <placideralph@gmail.com>
2022-07-24 17:30:49 -0400

    palindrome submittion

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/palindrome-linked-list/
+
+const isPalindrome = function(head) {
+    let index = 0;
+    let res = false;
+    while(head.length - index - 1 > index) {
+        let reversedIndex = head.length - index - 1;
+        if(head[reversedIndex] === head[index]) {
+            res = true;
+            index++;
+        } else {
+            res = false;
+            break;
+        }
+    }
+    return res;
+};
