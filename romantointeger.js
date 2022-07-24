f2fc4439447efc05fa539bd5771eedc36d885030
ralphZero <placideralph@gmail.com>
2022-07-24 18:27:03 -0400

    simple solution

--- romantointeger.js
+++ romantointeger.js
@@ -1,64 +1,33 @@
 // https://leetcode.com/problems/roman-to-integer/
 
-const romanToInt = function(numerals) {
+const romanToInt = function (numerals) {
     let sum = 0;
     numerals = numerals.split('');
-    for(let i = 0; i < numerals.length; i++) {
-        const romanNumeral = numerals[i];
-        const nextRomanNumeral = numerals[i+1];
-        if(romanNumeral === 'I' && nextRomanNumeral === 'V') {
-            //return 4 && increment i
-            sum+=4;
-            i++;
-        } else if(romanNumeral === 'I' && nextRomanNumeral === 'X') {
-            //return 9 && increment i
-            sum+=9;
-            i++;
-        } else if(romanNumeral === 'X' && nextRomanNumeral === 'L') {
-            //return 40 && increment i
-            sum+=40;
-            i++;
-        } else if(romanNumeral === 'X' && nextRomanNumeral === 'C') {
-            //return 90 && increment i
-            sum+=90;
-            i++;
-        } else if(romanNumeral === 'C' && nextRomanNumeral === 'D') {
-            //return 400 && increment i
-            sum+=400;
-            i++;
-        } else if(romanNumeral === 'C' && nextRomanNumeral === 'M') {
-            //return 900 && increment i
-            sum+=900;
-            i++;
+
+    const romanNumber = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+
+    for (let i = 0; i < numerals.length; i++) {
+
+        const dis = numerals[i];
+        const nxt = numerals[i + 1];
+
+        if(nxt && romanNumber[dis] < romanNumber[nxt]) {
+            sum = sum + (romanNumber[nxt] - romanNumber[dis])
+            i++
         } else {
-            //return the correct number
-            switch(romanNumeral) {
-                case 'V':
-                    sum+=5;
-                    break;
-                case 'X':
-                    sum+=10;
-                    break;
-                case 'L':
-                    sum+=50;
-                    break;
-                case 'C':
-                    sum+=100;
-                    break;
-                case 'D':
-                    sum+=500;
-                    break;
-                case 'M':
-                    sum+=1000;
-                    break;
-                default:
-                    sum++;
-                    break;
-            }
+           sum += romanNumber[dis];
         }
     }
 
     return sum;
 };
-let head = 'LVIII';
+let head = 'MCMXCIV';
 console.log(romanToInt(head));
